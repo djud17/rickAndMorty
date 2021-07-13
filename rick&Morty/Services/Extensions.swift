a376e7a7a38e3b661bf6cb0b59225695c6a21a72
djud17 <davidtonoyan10@rambler.ru>
2021-07-13 19:28:24 +0300

    Final touch

---
+++ rick&Morty/Services/Extensions.swift
@@ -0,0 +1,8 @@
+//
+//  Extensions.swift
+//  rick&Morty
+//
+//  Created by Давид Тоноян  on 23.05.2021.
+//
+
+import Foundation
---
+++ rick&Morty/Services/UrlAdresses.swift
@@ -0,0 +1,8 @@
+//
+//  UrlAdresses.swift
+//  rick&Morty
+//
+//  Created by Давид Тоноян  on 23.05.2021.
+//
+
+import Foundation
